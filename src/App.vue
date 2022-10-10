5af8a3542c6dfd06c52040ac01c4b03f696a2e15
Ernesto <ernesboyanos@hotmail.com>
2022-10-10 00:28:35 -0300

    ultimo antes deploy

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,77 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  
+  import {ref, computed} from 'vue';
+
+  const arrayFavoritos = ref([]);
+
+  const agregar = () => {
+      arrayFavoritos.value.push(counter.value);
+  }
+
+  const bloquearBoton = computed(()=>{
+      const numSearch  = arrayFavoritos.value.find((num)=>num === counter.value);
+      if(numSearch===0) return true;
+      return numSearch ? true : false;
+
+
+  });
+
+  var name = 'Ernesto';
+  const counter = ref(0);
+  const increment = () => {    
+    counter.value=counter.value+1;
+  }
+  const decrement = () => {    
+    counter.value=counter.value-1;
+  }
+  const reset = () => {    
+    counter.value=0;
+  }
+
+ const counterClass = computed(() => {
+        if (counter.value > 0) {
+            return 'positivos'
+        }
+        if (counter.value < 0) {
+            return 'negativos'
+        }
+        if (counter.value === 0) {
+            return 'zero'
+        }
+
+ })
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="container">
+  <h1> Hola {{name.toUpperCase()}}</h1>
+  <h2 :class="counterClass">{{counter}}</h2>
+  <div class="btn btn-group">
+  <button @click="increment" class="btn btn-success"> + </button>
+  <button @click="decrement" class="btn btn-danger"> - </button>
+  <button @click="reset" class="btn btn-secondary"> Reset </button>
+  <button :disabled="bloquearBoton" @click="agregar" class="btn btn-primary"> ADD </button>
+</div>
+  <hr>
+  {{arrayFavoritos}}
+  
+ </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style>
+h1 {
+  color: royalblue;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.positivos {
+  color: green;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.negativos {
+  color: red;
+}
+.zero {
+  color: peru;
 }
 </style>
